9a610b2e18b2d95fbe54309eecade00c19e5ae07
TomasOyarzun <oyarzun.tomas@gmail.com>
2018-11-04 14:31:33 -0300

    ADD: Faker gem, tests to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  Task.create(
+    name: "Tarea #{i + 1}",
+    photo: "https://i.blogs.es/60e3c6/maxresdefault-30-/450_1000.jpg"
+  )
+end
+ 10.times do |x|
+  User.create!(
+    email: "email#{x+1}@example.com",
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf",
+    name: Faker::Hobbit.character,
+    photo: "https://static1.funidelia.com/26258-f4_large/mascara-de-mario-bros-para-adulto.jpg"
+  )
+end
