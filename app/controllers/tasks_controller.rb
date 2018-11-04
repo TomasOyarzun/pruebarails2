8e86e5394b43aba76aa51edb08fe5d15bab8652f
TomasOyarzun <oyarzun.tomas@gmail.com>
2018-11-04 16:08:59 -0300

    ADD: methods in task controller. Deleted model task_user. Modified routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,36 @@
 class TasksController < ApplicationController
+  before_action :create_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    @names = @task.users.pluck(:name)
+  end
+
+  def complete
+    @task = Task.find(params[:id])
+    @task.users << current_user
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def incomplete
+    user = current_user
+    @task = Task.find(params[:id])
+    @task.users.delete(user)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def create_task
+    @task = Task.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
+  get 'tasks/:id', to: 'tasks#show', as:'task_show'
+  get 'tasks/:id/complete', to: 'tasks#complete', as:'tasks_complete'
+  delete 'tasks/:id/incomplete', to: 'tasks#incomplete', as:'tasks_incomplete'
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
